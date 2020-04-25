644a7307b4bcb63922dc1d19bc84ac093e5992bc
Manami Nakamura <manaminakamura812@gmail.com>
2020-04-26 00:33:03 +0900

    return json response

--- analysisapp/services.py
+++ analysisapp/services.py
@@ -1,12 +1,27 @@
 from konlpy.tag import Mecab
 from konlpy.utils import pprint
 from analysisapp.tags import tag_dict
+from hangul_romanize import Transliter
+from hangul_romanize.rule import academic
 
 def analyze(text):
     mecab = Mecab()
     poslist = mecab.pos(text)
-    return map(new_pos, poslist)
+    res = {
+      "text": text,
+      "translation": translate(text),
+      "romanized": romanize(text),
+      "tokens": list(map(new_pos, poslist)),
+    }
+    return res
 
 def new_pos(pos):
     new_tag = tag_dict[pos[1]]
     return (pos[0], new_tag)
+
+def translate(text):
+    return "日本語訳"
+
+def romanize(text):
+    transliter = Transliter(academic)
+    return transliter.translit(text)
--- analysisapp/views.py
+++ analysisapp/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 from analysisapp.services import analyze
 
 def show_analysis(request):
-    return HttpResponse(analyze(request.GET.get("text")))
+    response_data = analyze(request.GET.get("text"))
+    return HttpResponse(json.dumps(response_data, ensure_ascii=False),
+         content_type="application/json")
