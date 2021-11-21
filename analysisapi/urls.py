fcc18221587b7778118022667242dbda6ea0b2cd
manaminakamura <manaminakamura812@gmail.com>
2021-11-22 03:07:31 +0900

    ingressヘルスチェック対応

--- analysisapi/urls.py
+++ analysisapi/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from analysisapp import views
+from django.http import HttpResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('analysis/', views.show_analysis),
+    path('healthz/', lambda r: HttpResponse()),
+    path('', lambda r: HttpResponse()),
 ]
