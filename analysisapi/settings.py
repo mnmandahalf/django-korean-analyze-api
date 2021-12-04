d314f56110f740fac85dc4e0bdfef2911409c487
manaminakamura <manaminakamura812@gmail.com>
2021-12-05 01:17:49 +0900

    herokuお掃除

--- analysisapi/settings.py
+++ analysisapi/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -122,9 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
 
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
