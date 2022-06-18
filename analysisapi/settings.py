9995956436998985c899888c32551cb786205918
manaminakamura <manaminakamura812@gmail.com>
2022-06-19 01:01:01 +0900

    Revert "enable cors"
    This reverts commit 7bb9b078122bc88bf60ec812136ecb71a9a1d412.

--- analysisapi/settings.py
+++ analysisapi/settings.py
@@ -46,6 +46,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -120,3 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+    'https://yukkuri-127.com',
+    'https://goguma.jp'
+]
